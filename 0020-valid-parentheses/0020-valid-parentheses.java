69c157465a84bb21544a46225b630dafac757831
Shaik Esub <esubs229@gmail.com>
2026-09-20 17:32:42 +0530

    Time: 3 ms (85.93%), Space: 43.3 MB (40.99%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean isValid(String s) {
+       if(s.charAt(0)==')' || s.charAt(0) ==  ']' || s.charAt(0) == '}' ){
+        return false;
+       }
+       Stack<Character> st=new Stack<>();
+       for(int i=0;i<s.length();i++){
+        char ch = s.charAt(i);
+        if(ch == '(' || ch == '[' || ch == '{'){
+           st.push(ch);
+        }
+        else{
+            if(st.isEmpty()){
+    return false;
+}
+        char top=st.peek();
+         if((ch == ')' && top== '(' )|| (ch == ']' && top == '[' )|| (ch == '}' && top == '{')){
+            st.pop();
+            
+        }
+        else {
+            return false;
+        }
+        }
+
+       }
+       if(st.isEmpty()){
+        return true;
+       }
+       return false;
+    }
+}

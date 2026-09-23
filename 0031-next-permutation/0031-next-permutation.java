263164019250e7ebba951bbfdae8fabcfc85ce6d
Shaik Esub <esubs229@gmail.com>
2026-09-23 21:08:48 +0530

    Time: 0 ms (100%), Space: 44.3 MB (96.33%) - LeetHub

---
+++ 0031-next-permutation/0031-next-permutation.java
@@ -0,0 +1,41 @@
+class Solution {
+    public void nextPermutation(int[] nums) {
+        int n=nums.length;
+        int ind=-1;
+        for(int i=n-2;i>=0;i--){
+            if(nums[i]<nums[i+1]){
+                ind=i;
+                break;
+            }
+        }
+        if(ind==-1){
+            int start=0;
+            int end=n-1;
+            while(start<end){
+                int temp=nums[start];
+                nums[start]=nums[end];
+                nums[end]=temp;
+                start++;
+                end--;
+            }
+            return;
+        }
+        for(int i=n-1;i>ind;i--){
+            if(nums[i]>nums[ind]){
+                int temp=nums[i];
+                nums[i]=nums[ind];
+                nums[ind]=temp;
+                break;
+            }
+        }
+        int start=ind+1;
+            int end=n-1;
+            while(start<end){
+                int temp=nums[start];
+                nums[start]=nums[end];
+                nums[end]=temp;
+                start++;
+                end--;
+            }
+    }
+}
